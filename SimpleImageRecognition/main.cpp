909fcf03e13e860395578048a84d906102441cce
Эмиль Ившин <ivshin.emil@yandex.ru>
2023-08-31 20:22:08 +0300

    <Обязательно>

--- SimpleImageRecognition/main.cpp
+++ SimpleImageRecognition/main.cpp
@@ -10,9 +10,8 @@ using namespace cv;
 using namespace std;  //Для упрощения
 
 bool isImage(const filesystem::path& path) {
-    std::string extension = path.extension().string();
-    std::transform(extension.begin(), extension.end(), extension.begin(),
-        ::tolower);
+    string extension = path.extension().string();
+    transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
     return (extension == ".jpg" || extension == ".jpeg" || extension == ".png")
         ? true
         : false;
@@ -42,12 +41,12 @@ int main(int argc, char** argv) {
     CascadeClassifier car_classifier, person_classifier;
 
     if (!car_classifier.load(
-        "C:/opencv/sources/data/haarcascades/haarcascade_car.xml")) {
+        "C:/opencv/sources/data/haarcascades/haarcascade_fullbody.xml")) {
         cerr << "OpenCV model for cars recognition is not found.";
         return -1;
     }
     if (!person_classifier.load(
-        "C:/opencv/sources/data/haarcascades/haarcascade_body.xml")) {
+        "C:/opencv/sources/data/haarcascades/haarcascade_fullbody.xml")) {
         cerr << "OpenCV model for a person recognition is not found.";
         return -1;
     }
@@ -69,18 +68,20 @@ int main(int argc, char** argv) {
         person_classifier.detectMultiScale(img, person_rect, 1.1, 10);
 
         for (int i = 0; i < cars_rect.size(); i++) {
-            rectangle(img, cars_rect[i].tl(), cars_rect[i].br(), Scalar(0, 0, 255),
-                3);
+            rectangle(img, cars_rect[i].tl(), cars_rect[i].br(),
+                Scalar(0, 0, 255), 3);
             putText(img, "CAR", cars_rect[i].tl(), FONT_HERSHEY_SIMPLEX, 1,
                 Scalar(0, 0, 255), 2);
         }
-
+        
         for (int i = 0; i < person_rect.size(); i++) {
             rectangle(img, person_rect[i].tl(), person_rect[i].br(),
                 Scalar(0, 0, 255), 3);
             putText(img, "PERSON", cars_rect[i].tl(), FONT_HERSHEY_SIMPLEX, 1,
                 Scalar(0, 0, 255), 2);
         }
+
+        imwrite(path + "_copy" + entry.path().extension().string(), img);
     }
 
     return 0;
